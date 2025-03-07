36f720a4de94a63a6c1088c3ae8b12fb8ea443d4
Pankaj Jain <130095399+Pankajjain6395@users.noreply.github.com>
2025-03-07 23:10:17 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,38 @@
+s = int(input("Enter the value:"))
+
+print(s)
+if(s>=18):
+    print('you are a man')
+    print("hey man")
+else:
+    print("you are a boy")
+
+#problem 2 (take a number from user and give response 
+#            it is postive and nigative and zero)
+
+num = int(input("Enter the number:"))
+if num > 0:
+    print("postive")
+    print ("covide 19")
+elif num <0:
+    
+    print("negative")
+    print("not covide 19")
+else:
+    ("zero")   
+
+
+
+    #problem no 4
+
+marks = int(input("Enter the marks:"))
+if marks>=25:
+    print ("pass")
+    print ("congratulation you are parmoted")
+
+else:
+    print("Fail")
+    print("sorry batter luck next time")
+
+
+    
